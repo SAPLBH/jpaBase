3f83b5063fab6dc138b4985d9e4f78c11f97d354
I068605 <suvrajyoti.chatterjee@sap.com>
2016-12-06 23:49:06 +0530

    Second commit

---
+++ src/main/java/com/sap/lbh/JpaBaseApplication.java
@@ -0,0 +1,12 @@
+package com.sap.lbh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBaseApplication.class, args);
+	}
+}
