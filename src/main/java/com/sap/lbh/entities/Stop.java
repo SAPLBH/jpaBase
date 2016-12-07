9b1dfe9a11ee9a2e6238c55ad8ab75f0f2492591
I068605 <suvrajyoti.chatterjee@sap.com>
2016-12-07 10:10:22 +0530

    added 1:1 mapping

--- src/main/java/com/sap/lbh/entities/Stop.java
+++ src/main/java/com/sap/lbh/entities/Stop.java
@@ -1,14 +1,24 @@
 package com.sap.lbh.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Stop {
 	@Id
+	@Column(name="stopId")
 	private String stopId;
+	@Column(name="locId")
 	private String locId;
+	@Column(name="stopCat")
 	private String stopCat;
+	
+	@OneToOne
+	@JoinColumn(name="succId")
+	private Succ successor;
 	
 	public String getStopId() {
 		return stopId;
@@ -27,6 +37,12 @@ public class Stop {
 	}
 	public void setStopCat(String stopCat) {
 		this.stopCat = stopCat;
+	}
+	public Succ getSuccessor() {
+		return successor;
+	}
+	public void setSuccessor(Succ successor) {
+		this.successor = successor;
 	}
 	public Stop() {
 		super();
--- src/main/java/com/sap/lbh/entities/Succ.java
+++ src/main/java/com/sap/lbh/entities/Succ.java
@@ -1,13 +1,20 @@
 package com.sap.lbh.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Succ {
 	@Id
+	@Column(name="id")
 	private String id;
+	@Column(name="succId")
 	private String succId;
+	@OneToOne(mappedBy = "successor")
+	private Stop stop;
+	
 	public String getId() {
 		return id;
 	}
@@ -19,6 +26,12 @@ public class Succ {
 	}
 	public void setSuccId(String succId) {
 		this.succId = succId;
+	}
+	public Stop getStop() {
+		return stop;
+	}
+	public void setStop(Stop stop) {
+		this.stop = stop;
 	}
 	public Succ() {
 		super();
