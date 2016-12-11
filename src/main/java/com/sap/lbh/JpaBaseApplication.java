bfa1cd29873bc480d158219f97e6840965a01dfd
I068605 <suvrajyoti.chatterjee@sap.com>
2016-12-12 03:40:50 +0530

    enable multi module project

--- src/main/java/com/sap/lbh/JpaBaseApplication.java
+++ src/main/java/com/sap/lbh/JpaBaseApplication.java
@@ -3,7 +3,7 @@ package com.sap.lbh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class JpaBaseApplication {
 
 	public static void main(String[] args) {
