30b924fa60d642f67d4d4e10fb46f7c78eceef1d
I068605 <suvrajyoti.chatterjee@sap.com>
2016-12-12 18:51:09 +0530

    cf cloud deploy success

--- src/main/java/com/sap/lbh/JpaBaseApplication.java
+++ src/main/java/com/sap/lbh/JpaBaseApplication.java
@@ -1,9 +1,14 @@
 package com.sap.lbh;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({ "com.sap.lbh" })
 public class JpaBaseApplication {
 
 	public static void main(String[] args) {
